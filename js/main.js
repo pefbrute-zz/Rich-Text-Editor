db3f49c832aa406752faaa99d363cac2440ebc7a
pefbrute <proggraming@mail.ru>
2020-05-05 12:05:30 +0300

    +backbone

---
+++ js/main.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", function () {
+  document.getElementById("work-area").setAttribute("contenteditable", "true");
+});
+
+function format(command, value) {
+  document.execCommand(command, false, value);
+}
+
+function makeBold() {
+  document.getSelection();
+}
+
+function setUrl() {
+  var url = document.getElementById("txtFormatUrl").value;
+  var sText = document.getSelection();
+  document.execCommand(
+    "insertHTML",
+    false,
+    '<a href="' + url + '" target="_blank">' + sText + "</a>"
+  );
+  document.getElementById("txtFormatUrl").value = "";
+}
