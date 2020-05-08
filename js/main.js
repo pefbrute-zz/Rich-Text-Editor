82ae46f0db2df24e6c4140e568e62b85ce1771ea
pefbrute <proggraming@mail.ru>
2020-05-08 17:55:42 +0300

    +tests

--- js/main.js
+++ js/main.js
@@ -41,12 +41,35 @@ function insertTag2(tag) {
   }
 }
 
-function test() {
-  console.log(document.getSelection());
-  console.log(document.getSelection().getRangeAt(0));
-  console.log(document.getSelection().getRangeAt(0).cloneContents());
-  var u = document.createElement('u');
-  u.appendChild(document.getSelection().getRangeAt(0).cloneContents().children[0]);
-  document.getSelection().getRangeAt(0).insertNode(u);
+function test() 
+{
+  var selection = document.getSelection();
+  var constantRange = selection.getRangeAt(0);
+  var variableRange = selection.getRangeAt(0);
+  var clonedContents = constantRange.cloneContents();
+  
+  console.log(selection);
+  console.log(constantRange);
+  console.log(clonedContents);
+
+  //add function of focusing on the different part of range
+  variableRange.setStart(selection.anchorNode, 0);
+  variableRange.setEnd(selection.anchorNode, 2);
+
+  
+
+
+  // constantRange.setStart(constantRange.startContainer, 0);
+  // constantRange.setEnd(constantRange.endContainer, 2)
+
+  // var u = document.createElement('u');
+  // for (var i = 0; i < clonedContents.children.length - 1; i++){
+  //   u.appendChild(document.getSelection().getRangeAt(0).cloneContents().children[0].childNodes[i]);
+  // }
+
+  // u.appendChild(document.getSelection().getRangeAt(0).cloneContents().children[0]);
+  
+  // document.getSelection().getRangeAt(0).insertNode(u);
+  
   // console.log(document.getSelection().getRangeAt(0).extractContents());
 }
