d8b8cea0d62e91cee5e9a6c2ca1a6d9b2e559f8a
pefbrute <proggraming@mail.ru>
2020-05-11 11:03:35 +0300

    +text-align-functions

--- js/main.js
+++ js/main.js
@@ -17,16 +17,6 @@ function setUrl() {
   document.getElementById("txtFormatUrl").value = "";
 }
 
-function makeBold() {
-  var strongApplier, underlinedApplier, emphasizedApplier;
-  window.onload = function () {
-    rangy.init();
-    strongApplier = rangy.createClassApplier("strong");
-    underlinedApplier = rangy.createClassApplier("u");
-    emphasizedApplier = rangy.createClassApplier("em");
-  };
-}
-
 rangy.init();
 var strongApplier;
 strongApplier = rangy.createClassApplier("strong");
@@ -34,15 +24,85 @@ var underlinedApplier;
 underlinedApplier = rangy.createClassApplier("u");
 var emphasizedApplier;
 emphasizedApplier = rangy.createClassApplier("em");
+var alignRightApplier;
+alignRightApplier = rangy.createClassApplier("align-right");
+var alignLeftApplier;
+alignLeftApplier = rangy.createClassApplier("align-left");
+var alignCenterApplier;
+alignCenterApplier = rangy.createClassApplier("align-center");
+var alignJustifyApplier;
+alignJustifyApplier = rangy.createClassApplier("align-justify");
 
-function makeTag(tag) {
-  if (tag == "strong") {
+function addDropdown() {
+  var dropdown = document.getElementById("dropdown");
+  if (dropdown.className == "hidden") {
+    dropdown.className = "dropdown";
+  } else {
+    dropdown.className = "hidden";
+  }
+}
+
+function addClass(className) {
+  if (className == "strong") {
     strongApplier.toggleSelection();
   }
-  if (tag == "u") {
+  if (className == "u") {
     underlinedApplier.toggleSelection();
   }
-  if (tag == "em") {
+  if (className == "em") {
     emphasizedApplier.toggleSelection();
+  }
+  if (className == "align-left") {
+    alignleftApplier.toggleSelection();
+  }
+  if (className == "align-center") {
+    alignCenterApplier.toggleSelection();
+  }
+  if (className == "align-right") {
+    alignRightApplier.toggleSelection();
+  }
+  if (className == "align-justify") {
+    alignJustifyApplier.toggleSelection();
+  }
+}
+
+function addContainerClass(className) {
+  var selection = document.getSelection();
+  var range = selection.getRangeAt(0);
+  var wrongContainer = document.getElementById("sample-toolbar");
+  if (
+    selection.type != "Caret" &&
+    range.commonAncestorContainer != wrongContainer
+  ) {
+    var mainContainer = document.getElementById("work-area");
+    var selectionParent = selection.anchorNode;
+    var lastNode = selection.focusNode;
+    var startContainer = selection.getRangeAt(0).startContainer;
+    var endContainer = selection.getRangeAt(0).endContainer;
+    if (
+      selectionParent != startContainer &&
+      selection.focusNode != endContainer
+    ) {
+      selectionParent = selection.focusNode;
+      lastNode = selection.anchorNode;
+    }
+    while (lastNode.parentElement != mainContainer) {
+      lastNode = lastNode.parentElement;
+    }
+    do {
+      while (selectionParent.parentElement != mainContainer) {
+        selectionParent = selectionParent.parentElement;
+      }
+      if (selectionParent.className == className) {
+        selectionParent.className = "";
+      } else {
+        selectionParent.className = className;
+      }
+      selectionParent = selectionParent.nextSibling;
+    } while (
+      selectionParent.nextSibling != null &&
+      selectionParent.nextSibling != lastNode.nextElementSibling
+    );
+    // }
   }
 }
